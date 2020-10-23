355203dcd0655903d3397901fa5b9a9c85f58d79
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2020-10-23 22:55:18 +1100

    feat: add Product Screen in frontend

---
+++ app-frontend/src/screens/ProductScreen.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id);
+  return (
+    <>
+      <Link className="btn btn-primary my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item className="bg-light text-dark">
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-light text-dark">
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-light text-dark">
+              <h3>${product.price}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item className="bg-light text-dark">
+              {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card className="border-primary">
+            <ListGroup variant="flush">
+              <ListGroup.Item className="bg-light border-primary text-dark">
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-light border-primary text-dark">
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-light">
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
