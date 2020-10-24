8669ab0ac44e17e937b6f6942bfc19c1c511952b
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2020-10-24 18:09:44 +1100

    feat: add usermodel, productmodel and ordermodel

--- app-backend/models/userModel.js
+++ app-backend/models/userModel.js
@@ -1,24 +1,32 @@
-import { truncate } from 'fs';
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
+const { Schema, model } = mongoose;
 
-const userSchema = Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const User = model('User', userSchema);
+
+export default User;
