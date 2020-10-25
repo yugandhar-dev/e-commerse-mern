7fe705c6c66519b29f9f9d59567295b30e7f50b0
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2020-10-25 23:21:31 +1100

    feat: user and products sample data created

---
+++ app-backend/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Steven Rogers',
+    email: 'admin@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Thor Odinson',
+    email: 'thor@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Edward Stark',
+    email: 'stark@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
